364329f81cc4cda7f61f8b6f6f21cd0707ff6f3c
Abdiaziz Mire <90585700+Azizmire@users.noreply.github.com>
2026-07-29 16:57:46 -0500

    Add upload permission constants

---
+++ server/src/controllers/upload.permissions.ts
@@ -0,0 +1,1 @@
+export const UploadPermissions={UPLOAD_PROPERTY_IMAGE:'upload:property:image',UPLOAD_LEASE_DOCUMENT:'upload:lease:document',UPLOAD_MAINTENANCE_ATTACHMENT:'upload:maintenance:attachment'} as const;
