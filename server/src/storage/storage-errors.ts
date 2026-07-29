1047885b20f09657b3b96faa80186790c6ea00b1
Abdiaziz Mire <90585700+Azizmire@users.noreply.github.com>
2026-07-29 16:21:25 -0500

    Add storage error types

---
+++ server/src/storage/storage-errors.ts
@@ -0,0 +1,20 @@
+export class StorageError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'StorageError';
+  }
+}
+
+export class FileNotFoundError extends StorageError {
+  constructor(key: string) {
+    super(`File not found: ${key}`);
+    this.name = 'FileNotFoundError';
+  }
+}
+
+export class InvalidUploadError extends StorageError {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidUploadError';
+  }
+}
