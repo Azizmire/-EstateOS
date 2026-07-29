c9e39b24ae1c77def42e2f1cbe875f45b7f6c86b
Abdiaziz Mire <90585700+Azizmire@users.noreply.github.com>
2026-07-29 16:43:13 -0500

    Add upload route constants

---
+++ server/src/controllers/upload.route.names.ts
@@ -0,0 +1,5 @@
+export const UploadRoutes = {
+  propertyImages: '/properties/:propertyId/images',
+  leaseDocuments: '/leases/:leaseId/documents',
+  maintenanceAttachments: '/maintenance/:maintenanceId/attachments',
+} as const;
